801e8299065b92f54c9a680a9cef985afa5ecec6
yogapian <bklove0129@gmail.com>
2026-03-24 18:50:20 +0900

    Fix waiting state emoji visibility
    Restore previously removed left margin and emoji area.

--- src/App.jsx
+++ src/App.jsx
@@ -2237,7 +2237,7 @@ function AttendanceBoard({members,bookings,setBookings,setMembers,specialSchedul
                           
                           {/* 1. 왼쪽 여백 및 이모지 영역 (지워졌던 부분 복구!) */}
                          
-                          {isWaiting&&<span style={{fontSize:11,color:"transparent",flexShrink:0}}>⠿</span>}
+                          {isWaiting&&<span style={{fontSize:11,color:"#c8c0b0",flexShrink:0}}>⠿</span>}
                           <span style={{fontSize:15,flexShrink:0}}>{isOneday?"👤":GE[mem?.gender]||"🧘🏿"}</span>
                           
                           {/* 2. 이름 영역 */}
